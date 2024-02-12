d7ce5adbaeae0a736320d11958c1af6964660ebc
dpc-profile <daniel.pcastro11@gmail.com>
2024-02-12 13:57:29 -0300

    Gravando usuario no cache

--- srv/PessoaModel.cs
+++ srv/PessoaModel.cs
@@ -9,19 +9,19 @@ public class PessoaModel
     public DateOnly? Nascimento { get; set; }
     public IEnumerable<string>? Stack { get; set; }
 
-    public int Validacao()
+    public bool IsValid()
     {
         if (string.IsNullOrEmpty(Nome) || Nome.Length > 100 
             || string.IsNullOrEmpty(Apelido) || Apelido.Length > 32 
             || !Nascimento.HasValue)
-            return 1;
+            return false;
 
         foreach (string item in Stack ?? Enumerable.Empty<string>())
         {
             if (item.Length > 32 || item.Length == 0)
-                return 2;
+                return false;
         }
 
-        return 0;
+        return true;
     }
 }
--- srv/Program.cs
+++ srv/Program.cs
@@ -1,18 +1,56 @@
 using RinhaBachendV2;
 
+using System.Collections.Concurrent;
+using StackExchange.Redis;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 string dbString = builder.Configuration.GetConnectionString("DbConn");
 builder.Services.AddNpgsqlDataSource(dbString ?? "ERRO de connection string!!!");
 
-builder.Services.AddHostedService<InserePessoas>();
+string cacheString = builder.Configuration.GetConnectionString("CacheConn");
+
+
+// builder.Services.AddHostedService<InserePessoas>();
+
+builder.Services.AddSingleton(_ => new ConcurrentDictionary<string, PessoaModel>());
+builder.Services.AddSingleton(_ => new ConcurrentQueue<PessoaModel>());
+builder.Services.AddSingleton<IConnectionMultiplexer>( _ => ConnectionMultiplexer.Connect(cacheString));
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(cacheString);
+builder.Services.AddSingleton<IDatabase>(_ => redis.GetDatabase());
+
 
 var app = builder.Build();
 
-app.MapPost("/pessoas", async (PessoaModel pessoaModel) =>
+app.MapPost("/pessoas", async (
+    IDatabase cache, 
+    ConcurrentQueue<PessoaModel> processingQueue, 
+    PessoaModel pessoaModel) =>
 {
+    bool pessoaModelValid = pessoaModel.IsValid();
+    
+    // Fazer um try/catch com as mensagens mais exatas e ver no que dá
+    if (pessoaModelValid == false)
+        Results.UnprocessableEntity("Os dados da requisição estão invalidos");
 
-    return Results.Ok();
+    // apelido já cadastrado 422 - Unprocessable Entity/Content
+    RedisValue apelidoUsado = await cache.StringGetAsync(pessoaModel.Apelido);
+    if (!apelidoUsado.IsNull)
+        return Results.UnprocessableEntity("Apelido já cadastrado");
+
+    pessoaModel.Id = Guid.NewGuid();
+    var redisPayload = new List<KeyValuePair<RedisKey, RedisValue>>
+    {
+        new KeyValuePair<RedisKey, RedisValue>(new RedisKey(pessoaModel.Id.ToString()), new RedisValue(JsonSerializer.Serialize(pessoaModel))),
+        new KeyValuePair<RedisKey, RedisValue>(new RedisKey(pessoaModel.Apelido), RedisValue.EmptyString)
+    };
+
+    await cache.StringSetAsync(redisPayload.ToArray());
+    processingQueue.Enqueue(pessoaModel);
+
+    return Results.Created($"/pessoas/{pessoaModel.Id}", pessoaModel);
 });
 
 
